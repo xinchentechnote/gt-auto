4f252d66f8635fd7671faecff0f99b0be68f32c7
xinchen <xinchentechnote@163.com>
2025-08-03 12:40:52 +0800

    feat:remove unuse code

--- pkg/proto/szse_message_codec.go
+++ pkg/proto/szse_message_codec.go
@@ -1,7 +1,12 @@
 package proto
 
 import (
+	"bytes"
+	"fmt"
+	"strconv"
+
 	"github.com/xinchentechnote/fin-proto-go/codec"
+	szse_bin "github.com/xinchentechnote/fin-proto-go/szse-bin/messages"
 )
 
 // BinarySzseMessageCodec is a codec for encoding and decoding messages.
@@ -10,20 +15,58 @@ type BinarySzseMessageCodec struct{}
 
 // EncodeJSONMap implements MessageCodec.
 func (codec *BinarySzseMessageCodec) EncodeJSONMap(message map[string]interface{}) ([]byte, error) {
-	panic("unimplemented")
+	msgType, err := strconv.Atoi(message["MsgType"].(string))
+	if err != nil {
+		return nil, fmt.Errorf("unknown MsgType: %s", message["MsgType"].(string))
+	}
+	data, e := codec.JSONToStruct(message)
+	if e != nil {
+		return nil, fmt.Errorf("failed to encode message: %w", e)
+	}
+	return codec.Encode(uint32(msgType), data)
 }
 
 // JSONToStruct implements MessageCodec.
 func (codec *BinarySzseMessageCodec) JSONToStruct(jsonMap map[string]interface{}) (codec.BinaryCodec, error) {
-	panic("unimplemented")
+	msgType, err := strconv.Atoi(jsonMap["MsgType"].(string))
+	if err != nil {
+		return nil, fmt.Errorf("unknown MsgType: %s", jsonMap["MsgType"].(string))
+	}
+	message, err := szse_bin.NewSzseBinaryMessageByMsgType(uint32(msgType))
+	if err != nil {
+		return nil, err
+	}
+	err = ConvertMapToStruct(jsonMap, message)
+	if err != nil {
+		return nil, err
+	}
+	return message, nil
 }
 
 // Encode a message into a byte slice and prepends the message type and length.
 func (codec *BinarySzseMessageCodec) Encode(ext interface{}, message codec.BinaryCodec) ([]byte, error) {
-	panic("unimplemented")
+	// 将字符串 MsgType 转换为 int32
+	msgType := ext.(uint32)
+	szseBinary := &szse_bin.SzseBinary{
+		MsgType: msgType,
+		Body:    message,
+	}
+	var buf bytes.Buffer
+	err := szseBinary.Encode(&buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 // Decode a byte slice into a message.
 func (codec *BinarySzseMessageCodec) Decode(data []byte) (interface{}, codec.BinaryCodec, error) {
-	panic("unimplemented")
+	var szseBinary szse_bin.SzseBinary
+	var buf bytes.Buffer
+	buf.Write(data)
+	err := szseBinary.Decode(&buf)
+	if err != nil {
+		return nil, nil, err
+	}
+	return szseBinary.MsgType, szseBinary.Body, nil
 }
